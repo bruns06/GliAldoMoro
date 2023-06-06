7e896c326763703840835b922d9449b964f78e57
bruns06 <bruns2506@gmail.com>
2023-06-06 14:36:54 +0200

    sistemato testi Presenti

--- src/main/java/com/bruna/presentation/ControllerMVC.java
+++ src/main/java/com/bruna/presentation/ControllerMVC.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bruna.entities.Testo;
 import com.bruna.services.TestoService;
+import com.bruna.repo.FraseDAO;
+import com.bruna.repo.ParolaDAO;
 import com.bruna.repo.TestoDAO;
 
 @Controller
@@ -17,10 +19,17 @@ public class ControllerMVC {
 	private TestoService service;
 	@Autowired
 	private TestoDAO dao;
+	@Autowired
+	private FraseDAO fdao;
+	@Autowired
+	private ParolaDAO pdao;
+	
 	
 	@GetMapping("/testiPresenti")
 	public String showTesti (Model m) {
 		m.addAttribute("testi", dao.findAll());
+		m.addAttribute("numeroFrasi", fdao.count());
+		m.addAttribute("numeroParole", pdao.count());
 		return "testi.html";
 	}
 }
